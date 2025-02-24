e5e12e1fbe1203358b675d33cc61890917f0d40d
Alphonso Varela <alphonso.varela@islington.gov.uk>
2025-02-24 12:47:46 +0000

    feat: create new command for applying discount strategies

---
+++ DesignPatternsWorkshop.Application/Commands/AddDiscountCommand.cs
@@ -0,0 +1,34 @@
+﻿using DesignPatternsWorkshop.Application.DTOs;
+using DesignPatternsWorkshop.Application.Strategies;
+using DesignPatternsWorkshop.Domain.Strategies;
+
+namespace DesignPatternsWorkshop.Application.Commands;
+
+public class AddDiscountCommand : IPurchaseCommand
+{
+    #region properties
+    private PurchaseDTO _purchase;
+    private IDiscountStrategy _discount;
+    #endregion
+
+    #region constructor
+    public AddDiscountCommand(PurchaseDTO purchase, IDiscountStrategy discount)
+    {
+        _purchase = purchase;
+        _discount = discount;
+    }
+    #endregion
+
+    #region methods
+    public void Execute()
+    {
+        _purchase.SetDiscountStrategy(_discount);
+    }
+
+    public void Revert()
+    {
+        var noDiscount = new NoDiscountStrategy();
+        _purchase.SetDiscountStrategy(noDiscount);
+    }
+    #endregion
+}

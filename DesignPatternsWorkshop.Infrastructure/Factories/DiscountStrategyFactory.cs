c6364e4d837055cbe9ffa9c518908802bf27bdc8
Alphonso Varela <alphonso.varela@islington.gov.uk>
2025-02-27 12:10:58 +0000

    feat: apply different date specific discount with decorator via the UI

--- DesignPatternsWorkshop.Infrastructure/Factories/DiscountStrategyFactory.cs
+++ DesignPatternsWorkshop.Infrastructure/Factories/DiscountStrategyFactory.cs
@@ -17,11 +17,15 @@ public class DiscountStrategyFactory
             case "bundle":
                 return new BundleDiscountStrategy(value);
             case "birthday":
-                var strategy = new PercentageDiscountStrategy(value);
+                var birthdayStrategy = new PercentageDiscountStrategy(value);
                 // this would obviously be some form of validation logic
                 // returning us the birth date of our user, Jane Austen
                 var clientBirthday = new DateOnly(1775, 12, 16);
-                return new RestrictedDateDiscountDecorator(strategy, clientBirthday);
+                return new RestrictedDateDiscountDecorator(birthdayStrategy, clientBirthday);
+            case "workshop":
+                var workshopStrategy = new PercentageDiscountStrategy(value);
+                var workshopDate = new DateOnly(2025, 04, 10);
+                return new RestrictedDateDiscountDecorator(workshopStrategy, workshopDate);
             default:
                 throw new ArgumentException("Invalid discount strategy identifier");
         }

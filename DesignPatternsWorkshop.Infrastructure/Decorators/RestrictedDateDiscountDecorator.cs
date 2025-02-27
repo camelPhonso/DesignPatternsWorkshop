12aa1f0dcb85ba54cd41a265a02f9fe617538c50
Alphonso Varela <alphonso.varela@islington.gov.uk>
2025-02-27 09:48:41 +0000

    feat: add decorator for date-restricted discounts

---
+++ DesignPatternsWorkshop.Infrastructure/Decorators/RestrictedDateDiscountDecorator.cs
@@ -0,0 +1,34 @@
+﻿using DesignPatternsWorkshop.Application.Strategies;
+
+namespace DesignPatternsWorkshop.Infrastructure.Decorators;
+
+public class RestrictedDateDiscountDecorator : IDiscountStrategy
+{
+    #region properties
+    private IDiscountStrategy _strategy;
+    private DateOnly _validDate;
+    public string Name { get; }
+    #endregion
+
+    #region constructor
+    public RestrictedDateDiscountDecorator(IDiscountStrategy strategy, DateOnly validDate)
+    {
+        _strategy = strategy;
+        _validDate = validDate;
+        Name = strategy.Name;
+    }
+    #endregion
+
+    #region methods
+    public double ApplyDiscount(double amount)
+    {
+        var today = DateOnly.FromDateTime(DateTime.Today);
+        if(today.Month == _validDate.Month && today.Day == _validDate.Day)
+        {
+            return _strategy.ApplyDiscount(amount);
+        }
+
+        return amount;
+    }
+    #endregion
+}

169f1451501d3f5ca7a3917d82c4fa777069549a
Alphonso Varela <Alphonso.Varela@islington.gov.uk>
2025-05-02 12:52:04 +0100

    docs: add xml documentation for Match method in result struct

--- DesignPatternsWorkshop.Domain/Result/Result.cs
+++ DesignPatternsWorkshop.Domain/Result/Result.cs
@@ -25,6 +25,13 @@ public readonly struct Result<TResult, TError>
     public static implicit operator Result<TResult, TError>(TResult value) => new(value);
 
     public static implicit operator Result<TResult, TError>(TError error) => new(error);
-    
+   
+    /// <summary>
+    /// Matcher method that reinforces handling of both possible outcomes of the Result pattern.
+    /// The output final output will be wrapped in the unifying type passed to the method call.
+    /// </summary>
+    /// <typeparam name="U"></typeparam>
+    /// <param name="success"></param>
+    /// <param name="failure"></param>
     public U Match<U>(Func<TResult, U> success, Func<TError, U> failure) => IsSuccess ? success(_value!) : failure(_error!);
 }

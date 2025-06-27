b32245f2d676d88ffe987c3f8738a84ffc4b4e20
Alphonso Varela <Alphonso.Varela@islington.gov.uk>
2025-06-27 16:06:33 +0100

    feat: add observer pattern interfaces

---
+++ DesignPatternsWorkshop.Application/Observer/IObserver.cs
@@ -0,0 +1,8 @@
+﻿using DesignPatternsWorkshop.Application.DTOs;
+
+namespace DesignPatternsWorkshop.Application.Observer;
+
+public interface IObserver
+{
+    void Update(PurchaseDTO purchase);
+}
---
+++ DesignPatternsWorkshop.Application/Observer/ISubject.cs
@@ -0,0 +1,8 @@
+﻿namespace DesignPatternsWorkshop.Application.Observer;
+
+public interface ISubject
+{
+    void Attach(IObserver observer);
+    void Detach(IObserver obsserver);
+    void Notify();
+}
